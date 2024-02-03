32ecdda7f89159403f2f9d0b001907a2a5f376af
namiskuukkel <namiskuukkel@gmail.com>
2024-02-04 01:55:43 +0200

    Update web-app.js

--- web-app.js
+++ web-app.js
@@ -1,5 +1,5 @@
 const app = window.Telegram.WebApp;
-const tele = window.Telegraf;
+
 app.ready();
 app.expand();
 console.log(app.initData);
@@ -15,14 +15,7 @@ errorer.innerHTML = JSON.stringify(app.initDataUnsafe);
 window.addEventListener("error", e => setError(e.toString()));
 
 btn.addEventListener("click", () => {
-	var bot = new window.Telegraf('5000139198:AAErGQeD3g-_lzc5Vvrj8WkBaX3jBjtibdA', { telegram: { testEnv: true } });
-	bot.telegram.answerWebAppQuery(app.initDataUnsafe.query_id, {
-	id: "0",
-	type: "article",
-	title: "Hello Mini App!",
-	input_message_content: {
-		message_text: "This message was sent from answerWebAppQuery",
-	},
+	app.close();
 });
 
 reset.addEventListener("click", () => {
