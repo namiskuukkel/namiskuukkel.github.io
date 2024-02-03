23636afd4a106f13b90400aacd029a94acb476d3
namiskuukkel <namiskuukkel@gmail.com>
2024-02-03 20:32:45 +0200

    Update web-app.js

--- web-app.js
+++ web-app.js
@@ -1,5 +1,4 @@
 const app = window.Telegram.WebApp;
-const storage = app.CloudStorage;
 
 app.ready();
 app.expand();
@@ -11,42 +10,13 @@ const [errorer, btn, reset, count] = [
 	/** @type {HTMLParagraphElement} */ (document.getElementById("count")),
 ];
 
-/** @param e {any} */
-const setError = e =>
-	(errorer.innerText += "\n" + JSON.stringify(e, null, "\t"));
-
 window.addEventListener("error", e => setError(e.toString()));
 
-/** @param error {string | null} @param value {string | undefined} */
-const parse = (error, value) => (!error ? (value ? parseInt(value) : 0) : 0);
-
-storage.getItem("clicks", (error, value) => {
-	if (error) return setError({ error, type: typeof error, value });
-	btn.removeAttribute("disabled");
-
-	const c = parse(error, value);
-	count.innerText = `Clicked ${c} times`;
-});
-
 btn.addEventListener("click", () => {
-	storage.getItem("clicks", (error, value) => {
-		if (error) return setError({ error, type: typeof error, value });
-		const c = parse(error, value) + 1;
-		storage.setItem("clicks", String(c), (error, success) => {
-			if (error) return setError({ error, type: typeof error, success });
-			count.innerText = `Clicked ${c} times`;
-			btn.removeAttribute("disabled");
-		});
-	});
 	app.sendData(JSON.stringify({"moi": 1}));
 });
 
 reset.addEventListener("click", () => {
-	storage.removeItem("clicks", (error, success) => {
-		if (error) return setError({ error, type: typeof error, success });
-		count.innerText = `Count was reset!`;
-		btn.removeAttribute("disabled");
-	});
 });
 
 export {};
