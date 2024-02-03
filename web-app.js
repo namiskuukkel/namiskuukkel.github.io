15494f00112f805a4d38b01d2a1e189baf150c8f
namiskuukkel <namiskuukkel@gmail.com>
2024-02-03 21:07:03 +0200

    Update web-app.js

--- web-app.js
+++ web-app.js
@@ -3,6 +3,7 @@ const app = window.Telegram.WebApp;
 app.ready();
 app.expand();
 console.log(app.initData);
+
 const [errorer, btn, reset, count] = [
 	/** @type {HTMLParagraphElement} */ (document.getElementById("errors")),
 	/** @type {HTMLButtonElement} */ (document.getElementById("click")),
@@ -10,6 +11,7 @@ const [errorer, btn, reset, count] = [
 	/** @type {HTMLParagraphElement} */ (document.getElementById("count")),
 ];
 
+errorer.innerHTML = JSON.stringify(app.initData);
 window.addEventListener("error", e => setError(e.toString()));
 
 btn.addEventListener("click", () => {
